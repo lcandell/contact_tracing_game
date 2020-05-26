783ff3513d921e6b6ef3b594208ee9dd584e85a2
Jeff Candell <jcandell@wustl.edu>
2020-05-26 11:56:18 -0400

    contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,89 @@
 var theCanvas = document.getElementById("theCanvas");
 var theContext = theCanvas.getContext("2d");
 theContext.beginPath();
-theContext.arc(300, 50, 5, 0, 2*Math.PI);
+theContext.arc(300, 50, 5, 0, 2 * Math.PI);
 theContext.fillStyle = "blue";
 theContext.fill();
 var x = 300;
 var y = 50;
+const radius=50
 
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 class Person {
     constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.contacts = []
+        this.x = x;
+        this.y = y;
+        this.contacts = {}
     }
 
     //draw projectile
+    draw() {
+        theContext.beginPath();
+        theContext.arc(this.x, this.y, radius, 0, 2 * Math.PI);
+        theContext.fillStyle = "red";
+        theContext.fill();
+    }
 
     //move projectile
+    move() {
+        this.x += getRndInteger(-10, 10);
+        this.y += getRndInteger(-10, 10);
+        if (this.x < radius) {
+            this.x = radius;
+        } else if (this.x > theCanvas.width-radius) {
+            this.x = theCanvas.width-radius
+        }
+
+        if (this.y < radius) {
+            this.y = radius;
+        } else if (this.y > theCanvas.height-radius) {
+            this.y = theCanvas.height-radius
+        }
+        this.draw();
+    }
 
     //log contact
-  }
-
-
-function drawProjectile() {
-    theContext.clearRect(0, 0, theCanvas.width, theCanvas.height);
-    theContext.beginPath();
-    theContext.arc(x, y, 5, 0, 2*Math.PI);
-    theContext.fillStyle = "red";
-    theContext.fill();
+    addContact(id){
+        //const key= String(id)
+        if(this.contacts[id]){
+            this.contacts[id]++
+        }else{
+            this.contacts[id]=1
+        }
+    }
 }
 
-function moveProjectile() {
-    x += getRndInteger(-10, 10);
-    y += getRndInteger(-10, 10);
-    if(x < 0){
-        x = 0;
-    } else if (x > theCanvas.width) {
-        x = theCanvas.width
-    }
+const population = []
+population.push(new Person(100,100))
+population.push(new Person(200,200))
 
-    if(y < 0){
-        y = 0;
-    } else if (y > theCanvas.height) {
-        y = theCanvas.height
+function findDist(person1,person2){
+    return Math.sqrt(Math.pow(person1.x-person2.x,2)+Math.pow(person1.y-person2.y,2))
+}
+
+function logContacts(){
+    for(let i=0;i<population.length;i++){
+        for(let j=i+1;j<population.length;j++){
+            if(findDist(population[i],population[j])<=2*radius){
+                population[i].addContact(j)
+                population[j].addContact(i)
+                console.log('contact made')
+            }
+        }
     }
-    
-    drawProjectile();
-    window.setTimeout(moveProjectile, 1000/30);
 }
 
 
-moveProjectile();
+function drawCanvas(){
+    theContext.clearRect(0,0,theCanvas.width,theCanvas.height)
+    for(person of population){
+        person.move()
+    }
+    logContacts()
+    window.setTimeout(drawCanvas,1000/30)
+}
+
+drawCanvas()
